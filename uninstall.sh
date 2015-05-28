e870d16af6c2172c48ec27d3e6b18d0b347c0e04
Ian Armour <ian@linux.com>
2015-05-28 22:17:01 +0100

    Prettify
    Fixed.

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,13 @@
 #Author: ther1v3r
 #Date: 5/28/2015
 
+echo ""
+echo ""
 echo "Removing Upstart Config and Install directory"
 rm -R /opt/Pushbullet-BTN
 rm /etc/init/Pushbullet-BTN.conf
+echo ""
+echo ""
 echo "If you wish to remove nodejs and npm do so with apt-get -y remove nodejs npm"
+echo ""
+echo ""
